4b004a71d80a8a4b58e7fba33b271ce3c6dba2af
SSpectre <jordan.barron.hurst@gmail.com>
2026-05-20 17:28:11 -0400

    Updated sprite sheet offset numbers for special forms

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -675,7 +675,7 @@ const Dex = new class implements ModdedDex {
 			num = BattlePokedex[id].num;
 		}
 		if (num < 0) num = 0;
-		if (num > 898) num = 0;
+		if (num > 1025) num = 0;
 
 		if (window.BattlePokemonIconIndexes?.[id]) {
 			num = BattlePokemonIconIndexes[id];
