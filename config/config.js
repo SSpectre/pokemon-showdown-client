313c0fa9d6d5f3f9686307fdf394b3eae32fb637
SSpectre <jordan.barron.hurst@gmail.com>
2026-05-22 13:16:53 -0400

    Remove code blocking gen 7 teambuilder table population

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (48164592)";
+Config.version = "0.11.2 (4d8222cc)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
